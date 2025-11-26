8733af8f20cd45e6aa51c44c5cb0e455b6796358
Navy22-anchor <lss4583@gmail.com>
2025-11-26 12:28:24 +0900

    FEAT:토큰화

--- Embedding_TEXT/Tokenizing_TEXT.py
+++ Embedding_TEXT/Tokenizing_TEXT.py
@@ -1,7 +1,14 @@
+import re
 
 file_path= 'Embedding_TEXT/Text_Data/The_Verdict.txt'
 
 with open(file_path,'r',encoding='utf-8') as f:
     raw_text = f.read()
-print(len(raw_text))
-print(raw_text[:99])
+
+#토큰화시 결정해야할것 : 어떤 목적인지에 따라 토큰화(단어를 끊는 기준)이 달라진다.
+#모델 개발시 한국어에 대한 토큰화 기준을 찾아 볼것
+result = re.split(r'''([,.?!_"()\:']|--|\s)''',raw_text)
+
+result = [item for item in result if item.strip()]
+
+print(result[:30])
