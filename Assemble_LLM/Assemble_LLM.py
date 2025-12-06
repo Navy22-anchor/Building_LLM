d0ca72e03fe9360f5674401b83360ae6bfa15ba5
Navy22-anchor <lss4583@gmail.com>
2025-12-06 14:53:26 +0900

    FEAT:LayerNorm (층정규화) 추가

--- Assemble_LLM/Assemble_LLM.py
+++ Assemble_LLM/Assemble_LLM.py
@@ -1,14 +1,6 @@
 import torch.nn as nn
 import torch
-GPT_CONFIG_124M = {
-    "vocab_size": 50257,    # 어휘사전 크기
-    "context_length": 1024, # 문맥 길이
-    "emb_dim": 768,         # 임베딩 차원
-    "n_heads": 12,          # 어텐션 헤드 개수
-    "n_layers": 12,         # 층 개수
-    "drop_rate": 0.1,       # 드롭아웃 비율
-    "qkv_bias": False       # 쿼리, 키, 값을 만들 때 편향 포함 여부
-}
+
 
 class DummyGPTmodel(nn.Module):
     def __init__(self,cfg) -> None:
